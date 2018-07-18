451a71ade4419ff7b1b9b896131e320421126530
laddusiddu <laddusiddu@LAXMI>
2018-07-18 19:46:43 +0530

    pojo setter and getter

--- src/main/java/com/my/pojos/User.java
+++ src/main/java/com/my/pojos/User.java
@@ -1,9 +1,30 @@
 package com.my.pojos;
 
+
 public class User {
 	
 	public Integer userId;
 	public String userName;
 	public String userMobile;
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserMobile() {
+		return userMobile;
+	}
+	public void setUserMobile(String userMobile) {
+		this.userMobile = userMobile;
+	}
+	
+	
 
 }
